d83f53df0257e66356d6032f4d663a29c103b5ef
Leonardo Vinsen <leonardovinsen@gmail.com>
2021-07-04 14:55:49 +0700

    add behind rules export

---
+++ lib/letter/splice/behind/behind.dart
@@ -0,0 +1,11 @@
+library behind;
+
+export 'behind_empty.dart';
+export 'behind_is_a_chachilga.dart';
+export 'behind_is_ae_chachilga.dart';
+export 'behind_is_chinga_ehshig.dart';
+export 'behind_is_ehshig.dart';
+export 'behind_is_not_ehshig.dart';
+export 'behind_like_head_babnvr_ihb.dart';
+export 'behind_like_middle_banbvr_bagao.dart';
+export 'behind_not_empty.dart';
---
+++ lib/letter/splice/behind_rule_holder.dart
@@ -0,0 +1,22 @@
+import 'behind/behind.dart';
+
+class BehindRuleHolder {
+  static final BehindEmpty BEHIND_EMPTY = new BehindEmpty();
+  static final BehindNotEmpty BEHIND_NOT_EMPTY = new BehindNotEmpty();
+  static final BehindLikeMiddleBanbvrBagaO BEHIND_LIKE_MIDDLE_BANBVR_BAGA_O =
+      new BehindLikeMiddleBanbvrBagaO();
+
+  static final BehindIsEhshig BEHIND_IS_EHSHIG = new BehindIsEhshig();
+  static final BehindIsNotEhshig BEHIND_IS_NOT_EHSHIG = new BehindIsNotEhshig();
+  static final BehindIsAEChachilga BEHIND_IS_AE_CHACHILGA =
+      new BehindIsAEChachilga();
+
+  static final BehindLikeHeadBanbvrIhB BEHIND_LIKE_HEAD_BANBVR_IH_B =
+      new BehindLikeHeadBanbvrIhB();
+
+  static final BehindIsAChachilga BEHIND_IS_A_CHACHILGA =
+      new BehindIsAChachilga();
+
+  static final BehindIsChingaEhshig BEHIND_IS_CHINGA_EHSHIG =
+      new BehindIsChingaEhshig();
+}
