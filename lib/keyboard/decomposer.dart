aefdf8d9ac1cddce58fe36377c7ca649636a4d62
Leonardo Vinsen <leonardovinsen@gmail.com>
2021-07-04 17:00:13 +0700

    Add KeyboardLayout and Decomposer

--- lib/keyboard/decomposer.dart
+++ lib/keyboard/decomposer.dart
@@ -1,117 +1,119 @@
+import 'package:ime_mongol_package/keyboard/keyboard_layout.dart';
 
+class Decomposer {
+  final Node root;
 
-  class Decomposer {
-    final Node root;
+  Decomposer() : this.root = new Node() {
+    this._build();
+  }
 
-      Decomposer(): this.root = new Node() {
-        this.build();
+  void _build() {
+    if (KeyboardLayout.getKeys().isEmpty) {
+      return;
+    }
+    Node current;
+    String str;
+    Node endNode;
+    for (String key in KeyboardLayout.getKeys()) {
+      if (key.isEmpty) {
+        continue;
+      }
+      current = root;
+      str = key;
+      for (int i = 0; i != str.length; i++) {
+        String character = str.substring(i, i + 1);
+        if (!current.contain(character)) {
+          current.put(character);
+        }
+        if (i == str.length - 1) {
+          endNode = current.get(character)!;
+          endNode.isEnd = true;
+          endNode.key = key;
+        }
+        current = current.get(character)!;
+      }
+    }
+  }
+
+  List<List<String>> decompose(final String str) {
+    int time1 = DateTime.now().millisecondsSinceEpoch;
+    try {
+      return this._decompose([], str);
+    } finally {
+      print(
+          'decompose runtime: ${DateTime.now().millisecondsSinceEpoch - time1}ms');
+    }
+  }
+
+  List<List<String>> _decompose(
+      final List<String> prefixes, final String? str) {
+    List<List<String>> cases = [];
+    if (str == null || str.isEmpty) {
+      cases.add(prefixes);
+      return cases;
     }
 
-    private void build() {
-        if (CollectionUtils.isEmpty(KeyboardLayout.getKeys())) {
-            return;
-        }
-        Node current;
-        String str;
-        Node endNode;
-        for (String key : KeyboardLayout.getKeys()) {
-            if (Strings.isBlank(key)) {
-                continue;
-            }
-            current = root;
-            str = key;
-            for (int i = 0; i != str.length(); i++) {
-                Character character = str.charAt(i);
-                if (!current.contain(character)) {
-                    current.put(character);
-                }
-                if (i == str.length() - 1) {
-                    endNode = current.get(character);
-                    endNode.setEnd(true);
-                    endNode.setKey(key);
-                }
-                current = current.get(character);
-            }
-        }
+    List<Node>? matchedNodes = this._check(str);
+    if (matchedNodes == null || matchedNodes.isEmpty) {
+      cases.addAll(_decompose(prefixes, str.substring(1)));
+      return cases;
     }
 
-      List<List<String>> decompose(final String str) {
-        long time1 = Instant.now().toEpochMilli();
-        try {
-            return this.decompose(new ArrayList<>(8), str);
-        } finally {
-            logger.info("decompose run time:{}", Instant.now().toEpochMilli() - time1);
-        }
+    for (Node matchedNode in matchedNodes) {
+      List<String> newPrefixes = List.from(prefixes);
+      newPrefixes.add(matchedNode.key!);
+      cases.addAll(
+          _decompose(newPrefixes, str.substring(matchedNode.key!.length)));
     }
+    return cases;
+  }
 
-    private List<List<String>> decompose(final List<String> prefixes, final String str) {
-        List<List<String>> cases = new ArrayList<>(8);
-        if (Strings.isBlank(str)) {
-            cases.add(prefixes);
-            return cases;
-        }
-
-        List<Node> matchedNodes = this.check(str);
-        if (CollectionUtils.isEmpty(matchedNodes)) {
-            cases.addAll(decompose(prefixes, str.substring(1)));
-            return cases;
-        }
-        for (Node matchedNode : matchedNodes) {
-            List<String> newPrefixes = new LinkedList<>(prefixes);
-            newPrefixes.add(matchedNode.getKey());
-            cases.addAll(decompose(newPrefixes, str.substring(matchedNode.key.length())));
-        }
-        return cases;
+  List<Node>? _check(final String? str) {
+    if (str == null || str.isEmpty) {
+      return null;
     }
-
-    private List<Node> check(final String str) {
-        if (Strings.isBlank(str)) {
-            return null;
-        }
-        char c;
-        Node current = root;
-        List<Node> matchNodes = new LinkedList<>();
-        for (int i = 0; i != str.length(); i++) {
-            c = str.charAt(i);
-            current = current.get(c);
-            if (current != null && current.isEnd()) {
-                matchNodes.add(current);
-            }
-            if (current == null) {
-                break;
-            }
-        }
-        return matchNodes;
+    String c;
+    Node? current = root;
+    List<Node> matchNodes = [];
+    for (int i = 0; i != str.length; i++) {
+      c = str.substring(i, i + 1);
+      current = current?.get(c);
+      if (current != null && current.isEnd) {
+        matchNodes.add(current);
+      }
+      if (current == null) {
+        break;
+      }
     }
-
-    
+    return matchNodes;
+  }
 }
 
 class Node {
-         String? _key;
-          bool _isEnd = false;
-          final _children = Map<String, Node>();
+  String? _key;
+  bool _isEnd = false;
+  final _children = Map<String, Node>();
 
-          String? get key => this._key;
-          bool get isEnd => this._isEnd;
+  String? get key => this._key;
+  bool get isEnd => this._isEnd;
 
-          void set key(String? key) {
-            this.key = key;
-          }
+  void set key(String? key) {
+    this.key = key;
+  }
 
-          void set isEnd(bool isEnd) {
-            this.isEnd = isEnd;
-          }
+  void set isEnd(bool isEnd) {
+    this.isEnd = isEnd;
+  }
 
-          Node? get(String character) {
-            return _children[character];
-        }
+  Node? get(String character) {
+    return _children[character];
+  }
 
-          bool contain(String character) {
-            return this._children.containsKey(character);
-        }
+  bool contain(String character) {
+    return this._children.containsKey(character);
+  }
 
-          void put(String character) {
-            _children[character] = new Node();
-        }
-    }
+  void put(String character) {
+    _children[character] = new Node();
+  }
+}
--- lib/keyboard/keyboard_layout.dart
+++ lib/keyboard/keyboard_layout.dart
@@ -1,80 +1,90 @@
-  import 'package:ime_mongol_package/letter/letter.dart';
+import 'package:ime_mongol_package/letter/letter.dart';
+import 'package:ime_mongol_package/letter/letter_shape.dart';
 import 'package:ime_mongol_package/letter/letters.dart';
 
 class KeyboardLayout {
+  static const List<String> KEYS = [
+    "a",
+    "e",
+    "i",
+    "o",
+    "u",
+    "n",
+    "b",
+    "p",
+    "h",
+    "g",
+    "m",
+    "l",
+    "s",
+    "x",
+    "t",
+    "d",
+    "q",
+    "j",
+    "y",
+    "r",
+    "w",
+    "f",
+    "k",
+    "c",
+    "z",
+    "H",
+    "R",
+    "L",
+    "Z",
+    "C"
+  ];
 
-    static const List<String> KEYS = [
-            "a", "e", "i", "o", "u",
-            "n", "b", "p", "h", "g", "m", "l", "s", "x", "t", "d", "q", "j", "y", "r",
-            "w", "f", "k", "c", "z",
-            "H", "R", "L", "Z", "C"];
+  static Map<String, List<Letter>> keyMap = {
+    "a": Letters.A,
+    "e": Letters.E,
+    "i": Letters.I,
+    "o": Letters.O,
+    "u": Letters.U,
+    "n": Letters.N,
+    "b": Letters.B,
+    "p": Letters.P,
+    "h": Letters.H,
+    "g": Letters.G,
+    "m": Letters.M,
+    "l": Letters.L,
+    "s": Letters.S,
+    "x": Letters.X,
+    "t": Letters.T,
+    "d": Letters.D,
+    "q": Letters.Q,
+    "j": Letters.J,
+    "y": Letters.Y,
+    "r": Letters.R,
+    "w": Letters.W,
+    "f": Letters.F,
+    "k": Letters.K,
+    "c": Letters.C,
+    "z": Letters.Z,
+    "H": Letters.GALIG_H,
+    "R": Letters.GALIG_R,
+    "L": Letters.GALIG_L,
+    "Z": Letters.GALIG_ZH,
+    "C": Letters.GALIG_CH,
+  };
 
-    static const Map<String, List<Letter>> keyMap = new Map(
-      "a": Letters.A;
-    )
+  static List<LetterShape> get(String key, int letterLocation) {
+    List<Letter>? letterList = keyMap[key];
 
-
-    static {
-        keyMap.put("a", Lists.newArrayList(Letters.A));
-        keyMap.put("e", Lists.newArrayList(Letters.E));
-        keyMap.put("i", Lists.newArrayList(Letters.I));
-        keyMap.put("o", Lists.newArrayList(Letters.O));
-        keyMap.put("u", Lists.newArrayList(Letters.U));
-
-        keyMap.put("n", Lists.newArrayList(Letters.N));
-        keyMap.put("b", Lists.newArrayList(Letters.B));
-        keyMap.put("p", Lists.newArrayList(Letters.P));
-        keyMap.put("h", Lists.newArrayList(Letters.H));
-        keyMap.put("g", Lists.newArrayList(Letters.G));
-        keyMap.put("m", Lists.newArrayList(Letters.M));
-        keyMap.put("l", Lists.newArrayList(Letters.L));
-        keyMap.put("s", Lists.newArrayList(Letters.S));
-        keyMap.put("x", Lists.newArrayList(Letters.X));
-        keyMap.put("t", Lists.newArrayList(Letters.T));
-        keyMap.put("d", Lists.newArrayList(Letters.D));
-        keyMap.put("q", Lists.newArrayList(Letters.Q));
-        keyMap.put("j", Lists.newArrayList(Letters.J));
-        keyMap.put("y", Lists.newArrayList(Letters.Y));
-        keyMap.put("r", Lists.newArrayList(Letters.R));
-        keyMap.put("w", Lists.newArrayList(Letters.W));
-        keyMap.put("f", Lists.newArrayList(Letters.F));
-        keyMap.put("k", Lists.newArrayList(Letters.K));
-        keyMap.put("c", Lists.newArrayList(Letters.C));
-        keyMap.put("z", Lists.newArrayList(Letters.Z));
-        keyMap.put("H", Lists.newArrayList(Letters.GALIG_H));
-        keyMap.put("R", Lists.newArrayList(Letters.GALIG_R));
-        keyMap.put("L", Lists.newArrayList(Letters.GALIG_L));
-        keyMap.put("Z", Lists.newArrayList(Letters.GALIG_ZH));
-        keyMap.put("C", Lists.newArrayList(Letters.GALIG_CH));
+    if (letterList == null || letterList.isEmpty) {
+      return [];
     }
 
-      static List<LetterShape> get(String key, byte letterLocation) {
-        List<Letter> letterList = keyMap.get(key);
-        if (CollectionUtils.isEmpty(letterList)) {
-            return Collections.emptyList();
-        }
-        List<LetterShape> letterShapeList = Lists.newArrayListWithExpectedSize(16);
-        try {
-            LetterShape tmp;
-            for (Letter letter : letterList) {
-                for (Field declaredField : letter.getClass().getDeclaredFields()) {
-                    if (!declaredField.getType().equals(LetterShape.class)) {
-                        continue;
-                    }
-                    tmp = (LetterShape) declaredField.get(letter);
-                    if (tmp.getLocation() == letterLocation) {
-                        letterShapeList.add(tmp);
-                    }
-                }
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-            throw new RuntimeException("throw exception when get the letter shape");
-        }
-        return letterShapeList;
-    }
+    List<LetterShape> letterShapeList = [];
+    letterList.forEach((letter) {
+      letterShapeList.addAll(letter.getLetterShapes());
+    });
 
-      static Set<String> getKeys() {
-        return Collections.unmodifiableSet(new HashSet<>(KEYS));
-    }
+    return letterShapeList;
+  }
+
+  static Set<String> getKeys() {
+    return Set.from(KEYS);
+  }
 }
